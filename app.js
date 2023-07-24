8f6bfeb2b7ad99b4947f5b5ed8544c33b4274835
Theankur28 <ankurpurohit6056@gmail.com>
2023-07-24 23:06:24 +0530

    Added all files

---
+++ app.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true});
+
+const fruitSchema = new mongoose.Schema({
+  
+   name: String,
+   rating: Number,
+   review: String 
+
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+   name: "Apple", 
+   rating : 7,
+   review: "pretty solid as a fruit."
+});
+fruit.save();
+
+ 
